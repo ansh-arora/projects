e5d2d571b782567b91a9dfe2d891ead9873ebc0d
Ansh Arora <ansh.arora3098@gmail.com>
2018-07-07 22:23:15 +0530

    final

--- weather_app.py
+++ weather_app.py
@@ -2,12 +2,16 @@ import datetime
 import json
 import urllib.request
 from tkinter import *
+from tkinter import ttk
 import requests
 import re
-
+from PIL import ImageTk,Image
+cod=0
+city1=''
+country1=''
 i=-1
 def url_builder(city_name):
-    user_api = 'xxxxxxxxxxxxxxxxxxxxxxx'  # Obtain yours form: http://openweathermap.org/
+    user_api = 'xxxxxxxxxxxxxxxxxxxxxxxx'  # Obtain yours form: http://openweathermap.org/
     unit = 'metric'  # For Fahrenheit use imperial, for Celsius use metric, and the default is Kelvin.
     api = 'http://api.openweathermap.org/data/2.5/weather?q='     # Search for your city ID here: http://bulk.openweathermap.org/sample/city.list.json.gz
 
@@ -28,7 +32,7 @@ def time_converter(time):
     ).strftime('%I:%M %p')
     return converted_time
 
-def data_organizer(raw_api_dict):
+def data_organizer(raw_api_dict):          #organizes data for current day
     data = dict(
         city=raw_api_dict.get('name'),
         country=raw_api_dict.get('sys').get('country'),
@@ -43,30 +47,40 @@ def data_organizer(raw_api_dict):
         wind=raw_api_dict.get('wind').get('speed'),
         wind_deg=raw_api_dict.get('deg'),
         dt=time_converter(raw_api_dict.get('dt')),
-        cloudiness=raw_api_dict.get('clouds').get('all')
+        cloudiness=raw_api_dict.get('clouds').get('all'),
+        cod=raw_api_dict.get('cod')
     )
+    global city1,country1,cod
+    city1=data['city']
+    country1=data['country']
+    cod=data['cod']
     return data
 
 def url_builder2(city_name):
-    user_api = 'xxxxxxxxxxxxxxxxxxxx'  # Obtain yours form: http://openweathermap.org/
+    user_api = 'xxxxxxxxxxxxxxxxxxxxxx'  # Obtain yours form: http://openweathermap.org/
     unit = 'metric'  # For Fahrenheit use imperial, for Celsius use metric, and the default is Kelvin.
     api = 'http://api.openweathermap.org/data/2.5/forecast?q='  # Search for your city ID here: http://bulk.openweathermap.org/sample/city.list.json.gz
     full_api_url = api + str(city_name) + '&mode=json&units=' + unit + '&APPID=' + user_api
+    #full_api_url returns:
+    #http://api.openweathermap.org/data/2.5/forecast?q=jaipur&mode=json&units=metric&APPID=xxxxxxxxxxxxxxxxxxxx
     r = requests.get(url=full_api_url)
     json_data= r.json()
     list_items= json_data['list']
-    return list_items
+    return list_items  #returns a raw list of data of next 3 days
 
-next_days=[]
-def data_sep(list_items):
+
+def data_sep(list_items):      #method to seperate data for next 3 days
     next_days = []
     for items in list_items:
         matchObj = re.match(r"((\d+)-(\d+)-(\d+)) (00:00:00)",items['dt_txt'], re.M | re.I)
+
         if matchObj:
-            next_days.append(items)
+            matchObj1=re.sub(r' 00:00:00',"",matchObj.group())
+            if matchObj1!=str(datetime.date.today()):
+                next_days.append(items)
     return next_days
 
-def data_organizer2(raw_list):
+def data_organizer2(raw_list):      #organize data of next 3 days
     data = dict(
         date=re.sub(r' 00:00:00',"",raw_list[i]['dt_txt']),
         temp=raw_list[i]['main']['temp'],
@@ -83,17 +97,46 @@ def data_organizer2(raw_list):
     return data
 
 def data_output2(data):
-    cur_temp.config(text='Temperature:')
-    temp_value.config(text=str(data['temp']) + '\xb0' + 'C'+' ( '+data['date']+' )')
-    wind_speed.config(text=str(data['wind'])+' km/h')
-    temp_max.config(text=str(data['temp_max']) + '\xb0' + 'C')
-    pressure.config(text=str(data['pressure'])+' mb')
-    temp_min.config(text=str(data['temp_min']) + '\xb0' + 'C')
-    humidity.config(text=str(data['humidity']) + '%')
-    sky.config(text=data['sky'])
+
+    city_label.place(x=240)
+    Label(m,
+          text='    ______________________________________________________________________________________________________    ',
+          bg='white').place(x=195, y=70)
+
+    canvas.place(x=435, y=110)
+    if data['sky'] == 'Rain':
+        img1 = Image.open('rainy.png')
+
+    elif data['sky'] == 'Clouds':
+        img1 = Image.open('cloudy.png')
+
+    elif data['sky'] == 'Haze':
+        img1 = Image.open('fog.png')
+
+    else:
+        img1 = Image.open('sunny.png')
+        img1 = img1.resize((80, 80), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img1)
+
+    canvas.create_image(0, 0, anchor=NW, image=img)
+    canvas.image = img
+    temp_value.config(text=str(data['temp']) + '\xb0' + 'C')
+    temp_value.place(x=520, y=110)
+
+    city_data.config(text=city1 + ',' + country1 + '\n' + data['date'] + '\n' + '"' + data['sky'] + '"')
+    city_data.place(x=260, y=115)
+
+    temp_max_min.config(
+        text='Max Temperature: ' + str(data['temp_max']) + '\xb0' + 'C' + '\n' + 'Min Temperature: ' + str(
+            data['temp_min']) + '\xb0' + 'C')
+    temp_max_min.grid(row=8, column=1)
+
+    humidity.config(
+        text='Humidity: ' + str(data['humidity']) + '%' + '\n' + 'Pressure: ' + str(data['pressure']) + ' mb'+'\n'+'Wind Speed: '+str(data['wind'])+'km/h')
+    humidity.grid(row=8, column=2)
     previous.config(state=NORMAL,command=previous_button)
-    sunrise_time.config(text='N/A')
-    sunset_time.config(text='N/A')
+    sunrise_time.config(text='Sunrise Time: ' + 'N/A' + '\n' + 'Sunset Time: ' + 'N/A')
+    sunrise_time.grid(row=8, column=3)
 
 
 def next_button():
@@ -104,60 +147,50 @@ def next_button():
     city = city_entry.get()
     data_output2(data_organizer2(data_sep(url_builder2(city))))
 
-
+global img
 def data_output(data):
+    global cod
+    if cod==200:
+        city_label.place(x=220)
+        Label(m,text='    ______________________________________________________________________________________________________    ',bg='white').place(x=195,y=70)
 
-        city_lab = Label(m, text="City:")
-        city_lab.grid(row=5, column=0)
-        city_data.config(text=data['city']+','+data['country'])
-        city_data.grid(row=5, column=1)
+        canvas.place(x=455, y=110)
+        if data['sky']=='Rain':
+            img1 = Image.open('rainy.png')
 
-        cur_temp.config(text='Current Temperature:')
-        cur_temp.grid(row=6,column=0)
-        temp_value.config(text=str(data['temp'])+'\xb0' + 'C'+' ( '+str(datetime.date.today())+' )')
-        temp_value.grid(row=6,column=1)
-        temp_max_label=Label(m,text='Max Temperature:')
-        temp_max_label.grid(row=7,column=0)
-        temp_max.config(text=str(data['temp_max']) + '\xb0' + 'C')
-        temp_max.grid(row=7, column=1)
-        temp_min_label = Label(m, text='Min Temperature:')
-        temp_min_label.grid(row=8, column=0)
-        temp_min.config(text=str(data['temp_min']) + '\xb0' + 'C')
-        temp_min.grid(row=8, column=1)
+        elif data['sky']=='Clouds':
+            img1 = Image.open('cloudy.png')
 
-        humidity_lab=Label(m,text='Humidity:')
-        humidity_lab.grid(row=9,column=0)
-        humidity.config(text=str(data['humidity'])+'%')
-        humidity.grid(row=9,column=1)
-        pressure_lab=Label(m,text='Pressure:')
-        pressure_lab.grid(row=10,column=0)
-        pressure.config(text=str(data['pressure'])+' mb')
-        pressure.grid(row=10,column=1)
+        elif data['sky']=='Haze':
+            img1 = Image.open('fog.png')
 
-        sky_lab=Label(m,text='Sky:')
-        sky_lab.grid(row=11,column=0)
-        sky.config(text=data['sky'])
-        sky.grid(row=11,column=1)
+        else:
+            img1 = Image.open('sunny.png')
+            img1 = img1.resize((80, 80), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img1)
+        canvas.create_image(0, 0, anchor=NW, image=img)
+        canvas.image=img
+        temp_value.config(text=str(data['temp']) + '\xb0' + 'C')
+        temp_value.place(x=540, y=110)
 
-        wind_lab=Label(m,text='Wind Speed:')
-        wind_lab.grid(row=12,column=0)
-        wind_speed.config(text=str(data['wind'])+' km/h')
-        wind_speed.grid(row=12,column=1)
+        city_data.config(text=data['city']+','+data['country']+'\n'+'Today'+' ('+str(datetime.date.today())+')'+'\n'+'"'+data['sky']+'"')
+        city_data.place(x=210,y=115)
 
+        temp_max_min.config(text='Max Temperature: '+str(data['temp_max']) + '\xb0' + 'C'+'\n'+'Min Temperature: '+str(data['temp_min']) + '\xb0' + 'C')
+        temp_max_min.grid(row=8, column=1)
 
-        sunrise_lab.config(text='Sunrise Time:')
-        sunrise_lab.grid(row=13,column=0)
-        sunrise_time.config(text=data['sunrise'])
-        sunrise_time.grid(row=13,column=1)
-        sunset_lab.config(text='Sunset Time:')
-        sunset_lab.grid(row=14,column=0)
-        sunset_time.config(text=data['sunset'])
-        sunset_time.grid(row=14,column=1)
+        humidity.config(text='Humidity: '+str(data['humidity'])+'%'+'\n'+'Pressure: '+str(data['pressure'])+' mb'+'\n'+'Wind Speed: '+str(data['wind'])+'km/h')
+        humidity.grid(row=8,column=2)
+
+        sunrise_time.config(text='Sunrise Time: '+str(data['sunrise'])+'\n'+'Sunset Time: '+str(data['sunset']))
+        sunrise_time.grid(row=8,column=3)
 
         next.config(text='Next Day>>',command=next_button)
-        next.grid(row=15,column=3)
+        next.place(x=590,y=320)
         previous.config(text='<<Previous Day',state=DISABLED)
-        previous.grid(row=15,column=1)
+        previous.place(x=170,y=320)
+    else:
+        Label(m,text='Enter a valid city',font=('Berlin Sans FB',20),bg='white',fg='black')
 
 def previous_button():
     global i
@@ -174,29 +207,26 @@ def previous_button():
 
 
 try:
-    m = Tk(className='Weather Forecast')
-    city_label = Label(m,text="Enter a city: ")
-    city_label.grid(row=0,column=0)
-    city_entry = Entry(m)
-    city_entry.grid(row=0,column=1)
-    city_data = Label(m)
-    country_data= Label(m)
-    cur_temp=Label(m)
-    temp_value=Label(m)
-    temp_max = Label(m)
-    temp_min = Label(m)
-    humidity=Label(m)
-    pressure=Label(m)
-    sunrise_lab = Label(m)
-    sunrise_time = Label(m)
-    sunset_lab = Label(m)
-    sunset_time = Label(m)
-    sky=Label(m)
-    wind_speed=Label(m)
-    show = Button(m,text="Show",command=previous_button)
-    show.grid(row=1, column=1)
-    next=Button(m)
-    previous=Button(m)
+    m = Tk(className=' Weather Forecast')
+    m.config(bg='white')
+    m.geometry("850x400")
+    m.grid_columnconfigure(6, minsize=100)
+    m.grid_columnconfigure(0, minsize=120)
+    m.grid_rowconfigure(8, minsize=400)
+    city_label = Label(m,text="Enter City: ",font=('Berlin Sans FB',15),bg='white',fg='black')
+    city_label.grid(row=0,column=1)
+    city_entry = Entry(m,font=('Berlin Sans FB',15),bg='white',fg='black',highlightthickness=1,highlightbackground='black')
+    city_entry.grid(row=0,column=2)
+    city_data = Label(m,font=('Berlin Sans FB',20),bg='white',fg='black')
+    temp_value=Label(m,font=('Berlin Sans FB',40),bg='white',fg='black')
+    temp_max_min = Label(m,font=('Berlin Sans FB',15),bg='white',fg='black')
+    humidity=Label(m,font=('Berlin Sans FB',15),bg='white',fg='black')
+    sunrise_time = Label(m,font=('Berlin Sans FB',15),bg='white',fg='black')
+    show = Button(m,text="Show",command=previous_button,font=('Berlin Sans FB',15),bg='white',fg='black')
+    show.grid(row=1, column=2)
+    canvas = Canvas(m, width=70, height=70, bg='white', highlightbackground='white')
+    next=Button(m,font=('Berlin Sans FB',15),bg='white',fg='black')
+    previous=Button(m,font=('Berlin Sans FB',15),bg='white',fg='black')
     m.mainloop()
 except IOError:
     print('no internet')
